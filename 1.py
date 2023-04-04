fb29c18216f9870788d3b6165aed779eb3269bb9
1NOEVSA1 <120162701+1NOEVSA1@users.noreply.github.com>
2023-04-04 16:39:34 +0900

    Update 1.py
    123

--- 1.py
+++ 1.py
@@ -1,120 +1,122 @@
 import telebot
-import time
-import random
+import matplotlib.pyplot as plt
+import numpy as np
+from numexpr import evaluate
 from sympy import symbols, solve, parse_expr, simplify
 from sympy.parsing.sympy_parser import standard_transformations, implicit_multiplication_application
 
 bot = telebot.TeleBot('5187622946:AAHdoul6bLiS7aAqC0oQdh1l2pyylk7R6RY')
+error = """Уважаемый пользователь, к сожалению, ваш запрос не может быть выполнен из-за некорректного ввода данных. Пожалуйста, проверьте правильность введенной информации и повторите попытку."""
 
 
 # команда /start
 @bot.message_handler(commands=['start'])
 def start_command(message):
+    bot.send_message(message.chat.id, f"""Здравствуйте, {message.from_user.first_name}! Рады приветствовать
+     Вас в нашем телеграмм боте. Мы создали его, чтобы облегчить Вам жизнь и сделать её более интересной и продуктивной.
+      Здесь Вы можете получить доступ к множеству полезных функций, а также узнать много всего нового. Мы надеемся, что
+       использование нашего бота станет для Вас приятным и удобным, и Вы найдете здесь все, что Вам нужно.
+        Желаем приятного использования нашего телеграмм бота!""")
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
     keyboard.row('/math', '/history')
-    bot.send_message(message.chat.id, 'Выберите функцию:', reply_markup=keyboard)
+    bot.send_message(message.chat.id, 'Выберите кнопку:', reply_markup=keyboard)
 
 
 # команда /dice
 @bot.message_handler(commands=['math'])
 def dice_function(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.row('решение уравнений', 'упрощение выражений')
-    keyboard.row('рисование графиков функций', 'решение неравенств')
-    keyboard.row('вернуться назад')
-    bot.send_message(message.chat.id, 'Выберите кнопку', reply_markup=keyboard)
+    keyboard.row('Решение уравнений', 'Упрощение выражений')
+    keyboard.row('Построение графиков функций', 'Решение неравенств')
+    keyboard.row('Вернуться назад')
+    bot.send_message(message.chat.id, 'Выберите кнопку:', reply_markup=keyboard)
     bot.register_next_step_handler(message, dice_roll)
 
 
 def dice_roll(message):
-    if message.text == 'решение уравнений':
+    if message.text == 'Решение уравнений':
         bot.send_message(message.chat.id, 'Напишите уравнение:')
         bot.register_next_step_handler(message, symp)
-    elif message.text == 'упрощение выражений':
+    elif message.text == 'Упрощение выражений':
         bot.send_message(message.chat.id, 'Напишите выражение:')
         bot.register_next_step_handler(message, symp1)
-    elif message.text == 'рисование графиков функций':
-        result = str(random.randint(1, 20))
-        bot.send_message(message.chat.id, 'Выпало число: ' + result)
-        dice_function(message)
-    elif message.text == 'решение неравенств':
+    elif message.text == 'Построение графиков функций':
+        bot.send_message(message.chat.id, 'Напишите функцию:')
+        bot.register_next_step_handler(message, send_plot)
+    elif message.text == 'Решение неравенств':
         bot.send_message(message.chat.id, 'Напишите неравенство:')
         bot.register_next_step_handler(message, symp2)
-    elif message.text == 'вернуться назад':
+    elif message.text == 'Вернуться назад':
         start_command(message)
+
+
+def send_plot(message):
+    global error
+    try:
+        expression = message.text.replace(' ', '').replace('y=', '').replace('^', '**')
+        print(expression)
+        x = np.linspace(-10, 10, 1000)
+        y = evaluate(expression)
+        fig, ax = plt.subplots()
+        ax.plot(x, y)
+        ax.set_title(f"График функции {expression}")
+        fig.savefig('plot.png')
+        bot.send_photo(message.chat.id, open('plot.png', 'rb'),
+                       caption=f"""Уважаемый {message.from_user.first_name}, представляем вашему вниманию график функции {expression}. Надеемся, что он поможет вам лучше понять и проанализировать данную функцию. Желаем успешных вычислений!""")
+        dice_function(message)
+    except:
+        bot.send_message(message.chat.id, error)
+        dice_function(message)
 
 
 def map_operations(formula_str):
     return formula_str.replace("^", "**").replace("=", "-")
 
+
 def symp2(message):
+    global error
     try:
         transformations = (standard_transformations + (implicit_multiplication_application,))
         f = parse_expr(map_operations(message.text), transformations=transformations)
         roots = solve(f)  # <-- вернуть все корни уравнения в виде списка
-        bot.send_message(message.chat.id, f'Ответ: {roots}')
+        bot.send_message(message.chat.id,
+                         f"""Уважаемый {message.from_user.first_name}, мы рады предоставить вам ответ на ваш запрос по неравенству. Наш алгоритм обработал ваш запрос и нашёл решение: {roots}. Мы надеемся, что наш ответ поможет вам добиться желаемого результата. Желаем вам успехов в решении математических задач!""")
         dice_function(message)
     except Exception:
-        bot.send_message(message.chat.id, f'Ошибка')
+        bot.send_message(message.chat.id, error)
         dice_function(message)
 
+
 def symp1(message):
+    global error
     try:
         bot.send_message(message.chat.id,
-                         f'Упрощённое выражение: '
-                         f'{simplify(parse_expr(message.text.replace(")(", ")*(").replace("^", "**")))}')
+                         f"""Уважаемый {message.from_user.first_name}, мы рады предоставить вам ответ на ваш запрос. Наш алгоритм обработал ваш запрос и нашёл решение: {simplify(parse_expr(message.text.replace(")(", ")*(").replace("^", "**")))}. Мы надеемся, что наш ответ поможет вам добиться желаемого результата. Желаем вам успехов в решении математических задач!""")
         dice_function(message)
     except Exception:
-        bot.send_message(message.chat.id, f'Ошибка')
+        bot.send_message(message.chat.id, error)
         dice_function(message)
 
 
 def symp(message):
+    global error
     try:
-        print(message.text)
         transformations = (standard_transformations + (implicit_multiplication_application,))
         f = parse_expr(map_operations(message.text), transformations=transformations)
         roots = [str(x) for x in solve(f)]  # <-- вернуть все корни уравнения в виде списка
         if len(roots) > 1:
-            bot.send_message(message.chat.id, f'Ответ: {", ".join(roots)}')
+            bot.send_message(message.chat.id,
+                             f"""Уважаемый {message.from_user.first_name}, предоставляем вам корни уравнения: {", ".join(roots)}, которые вы запрашивали. Мы надеемся, что данная информация поможет вам более глубоко изучить и проанализировать вашу задачу. Желаем вам удачи в решении вычислительных задач!""")
         elif len(roots) == 0:
-            bot.send_message(message.chat.id, f'У уравнения либо нет решения или корнем является любое число')
+            bot.send_message(message.chat.id,
+                             f"""Уважаемый {message.from_user.first_name}, мы рады сообщить, что наш алгоритм успешно обработал ваш запрос и вычислил, что корень уравнения может быть любым числом или не иметь решения. Мы надеемся, что наш ответ поможет вам понять особенности данного уравнения и продвинуться в решении математических задач. Желаем вам успехов и легкости в изучении математики!""")
         else:
-            bot.send_message(message.chat.id, f'Ответ: {roots[0]}')
+            bot.send_message(message.chat.id,
+                             f'f"""Уважаемый {message.from_user.first_name}, предоставляем вам корни уравнения: {str(roots[0])}, которые вы запрашивали. Мы надеемся, что данная информация поможет вам более глубоко изучить и проанализировать вашу задачу. Желаем вам удачи в решении вычислительных задач!"""')
         dice_function(message)
     except Exception:
-        bot.send_message(message.chat.id, f'Ошибка')
+        bot.send_message(message.chat.id, error)
         dice_function(message)
-
-
-# команда /timer
-@bot.message_handler(commands=['timer'])
-def timer_function(message):
-    keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.row('30 секунд', '1 минута')
-    keyboard.row('5 минут', 'вернуться назад')
-    bot.send_message(message.chat.id, 'Выберите время:', reply_markup=keyboard)
-    bot.register_next_step_handler(message, timer)
-
-
-def timer(message):
-    if message.text == 'вернуться назад':
-        start_command(message)
-    elif message.text == '30 секунд':
-        bot.send_message(message.chat.id, 'Засек 30 секунд')
-        time.sleep(30)
-        bot.send_message(message.chat.id, '30 секунд истекло')
-        close_keyboard(message)
-    elif message.text == '1 минута':
-        bot.send_message(message.chat.id, 'Засек 1 минуту')
-        time.sleep(60)
-        bot.send_message(message.chat.id, '1 минута истекла')
-        close_keyboard(message)
-    elif message.text == '5 минут':
-        bot.send_message(message.chat.id, 'Засек 5 минут')
-        time.sleep(300)
-        bot.send_message(message.chat.id, '5 минут истекло')
-        close_keyboard(message)
 
 
 def close_keyboard(message):
